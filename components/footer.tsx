60cc0305e7125f5e17b7a097fd0dd83d9b51b4b8
muhammadhanief <haniefm19@gmail.com>
2023-09-06 08:41:14 +0700

    push link

--- components/footer.tsx
+++ components/footer.tsx
@@ -2,12 +2,12 @@ import React from "react";
 
 const footer = () => {
   return (
-    <footer className="bg-neutral-200 text-center dark:bg-neutral-700 lg:text-left">
+    <footer className="bg-neutral-200 text-center bg-black lg:text-left">
       <div className="p-4 text-center text-neutral-700 dark:text-neutral-200">
         © 2023 Copyright :
         <a
           className="px-2 text-neutral-800 dark:text-neutral-400"
-          href="https://tailwind-elements.com/"
+          href="https://tanahdatarkab.bps.go.id/"
         >
           BPS Kabupaten Tanah Datar
         </a>
